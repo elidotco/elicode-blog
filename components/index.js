13d40dcb0cce64b3624cc807bd8be7ad738b0856
elidotco <79024328+elidotco@users.noreply.github.com>
2022-10-22 15:27:58 +0100

    bug fixing

--- components/index.js
+++ components/index.js
@@ -11,3 +11,4 @@ export { default as Author } from "./Author";
 
 export { default as FeaturedPostCard } from "./FeaturedPostCard";
 export { default as AdjacentPostCard } from "./AdjacentPostCard";
+export { default as Loader } from "./Loader";
