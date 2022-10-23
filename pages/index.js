2d9165046ae05da360693b992312695b46ada889
elidotco <79024328+elidotco@users.noreply.github.com>
2022-10-23 21:24:34 +0100

    lan setting

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home({ posts }) {
         <meta name="description" content="Made by elicode" />
         <meta name="robots" content="all" />
         <link rel="icon" href="/favicon.ico" />
+        <html lang={english} />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
