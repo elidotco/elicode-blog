06fee8a9f1626c1f2af40437ef9d3d40c4e5c0d8
Ebenezer Ametepeh <79024328+elidotco@users.noreply.github.com>
2022-11-27 15:20:33 +0000

    ddsss

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,11 @@ export default function Home({ posts }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ req, res }) {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59"
+  );
   const posts = (await getPosts()) || [];
 
   return {
