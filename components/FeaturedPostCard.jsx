0fa714049ad2c785133d5bc91aa9fd909e6a7aef
Ebenezer Ametepeh <79024328+elidotco@users.noreply.github.com>
2022-11-28 15:31:19 +0000

    dffu

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -14,9 +14,6 @@ const FeaturedPostCard = ({ post }) => (
         alt={post.title}
         className="w-full relative align-middle rounded-lg object-cover"
         layout="fill"
-        sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
         priority={true}
       />
     </div>
